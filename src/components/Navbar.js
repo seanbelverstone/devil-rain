257b72f8cfbe7af59d2ea0675026f89cd8c413d9
Sean Belverstone <seanbelverstone@gmail.com>
2023-08-27 12:30:13 -0500

    added a highlight to the navbar, added more styling.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,34 @@
 import React, { useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import './css/Navbar.css';
 
 function Navbar() {
+	const location = useLocation();
 	useEffect(() => {
 		setHighlight();
-	}, [window.location.pathname]);
+	}, [location.pathname]);
 
 
 	const setHighlight = () => {
-		console.log(window.location.pathname);
+		Array.from(document.getElementsByClassName('navicon')).forEach(item => item.classList.remove('active'));
+		const iconId = location.pathname === '/' ? 'home' : location.pathname.split('/')[1];
+		const icon = document.getElementById(iconId);
+		icon && icon.classList.add('active');
 	};
 
 	return (
 		<div className="navbar">
 			<NavLink className="navlinks" to="/">
-				<span className="material-symbols-outlined navicon" id="homeIcon">home</span>
+				<span className="material-symbols-outlined navicon" id="home" alt="home">home</span>
 			</NavLink>
 			<NavLink className="navlinks" to="/videos">
-				<span className="material-symbols-outlined navicon" id="videoIcon">movie</span>
+				<span className="material-symbols-outlined navicon" id="videos">movie</span>
 			</NavLink>
 			<NavLink className="navlinks" to="/contact">
-				<span className="material-symbols-outlined navicon" id="contactIcon">mail</span>
+				<span className="material-symbols-outlined navicon" id="contact">mail</span>
 			</NavLink>
 			<NavLink className="navlinks" to="/merch">
-				<span className="material-symbols-outlined navicon" id="merchIcon">shopping_cart</span>
+				<span className="material-symbols-outlined navicon" id="merch">shopping_cart</span>
 			</NavLink>
 		</div>
 	);
