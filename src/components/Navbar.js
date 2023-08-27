28c80a23809d0cf113dc6a8379a52437314c22db
Sean Belverstone <seanbelverstone@gmail.com>
2023-08-27 12:43:02 -0500

    added a hover animation to the navlinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
+import facebookIcon from '../assets/facebookLogo.png';
 import './css/Navbar.css';
 
 function Navbar() {
@@ -17,20 +18,25 @@ function Navbar() {
 	};
 
 	return (
-		<div className="navbar">
-			<NavLink className="navlinks" to="/">
-				<span className="material-symbols-outlined navicon" id="home" alt="home">home</span>
-			</NavLink>
-			<NavLink className="navlinks" to="/videos">
-				<span className="material-symbols-outlined navicon" id="videos">movie</span>
-			</NavLink>
-			<NavLink className="navlinks" to="/contact">
-				<span className="material-symbols-outlined navicon" id="contact">mail</span>
-			</NavLink>
-			<NavLink className="navlinks" to="/merch">
-				<span className="material-symbols-outlined navicon" id="merch">shopping_cart</span>
-			</NavLink>
-		</div>
+		<>
+			<div className="navbar">
+				<NavLink className="navlinks" to="/">
+					<span className="material-symbols-outlined navicon" id="home" alt="home">home</span>
+				</NavLink>
+				<NavLink className="navlinks" to="/videos">
+					<span className="material-symbols-outlined navicon" id="videos">movie</span>
+				</NavLink>
+				<NavLink className="navlinks" to="/contact">
+					<span className="material-symbols-outlined navicon" id="contact">mail</span>
+				</NavLink>
+				<NavLink className="navlinks" to="/merch">
+					<span className="material-symbols-outlined navicon" id="merch">shopping_cart</span>
+				</NavLink>
+			</div>
+			<a href="https://www.facebook.com/groups/544578629743391" target="_blank" rel="noreferrer">
+				<img src={facebookIcon} alt="The Facebook icon" id="facebookIcon"/>
+			</a>
+		</>
 	);
 }
   
