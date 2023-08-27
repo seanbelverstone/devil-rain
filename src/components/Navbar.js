eac2f0b992ab79fb9b019acf1f82f7d9b342a09a
Sean Belverstone <seanbelverstone@gmail.com>
2023-08-27 12:13:12 -0500

    added some styling including the creation of main theme colors. Added navlinks to the navbar and tested that they work

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import contactIcon from '../assets/icons/contactIcon.png';
-import homeIcon from '../assets/icons/homeIcon.png';
-import merchIcon from '../assets/icons/merchIcon.png';
-import videoIcon from '../assets/icons/videoIcon.png';
+import React, { useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import './css/Navbar.css';
 
 function Navbar() {
+	useEffect(() => {
+		setHighlight();
+	}, [window.location.pathname]);
+
+
+	const setHighlight = () => {
+		console.log(window.location.pathname);
+	};
+
 	return (
 		<div className="navbar">
-			<img src={homeIcon} />
-			<img src={videoIcon} />
-			<img src={contactIcon} />
-			<img src={merchIcon} />
+			<NavLink className="navlinks" to="/">
+				<span className="material-symbols-outlined navicon" id="homeIcon">home</span>
+			</NavLink>
+			<NavLink className="navlinks" to="/videos">
+				<span className="material-symbols-outlined navicon" id="videoIcon">movie</span>
+			</NavLink>
+			<NavLink className="navlinks" to="/contact">
+				<span className="material-symbols-outlined navicon" id="contactIcon">mail</span>
+			</NavLink>
+			<NavLink className="navlinks" to="/merch">
+				<span className="material-symbols-outlined navicon" id="merchIcon">shopping_cart</span>
+			</NavLink>
 		</div>
 	);
 }
