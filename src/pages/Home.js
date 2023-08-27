f90422419780eb058b85205def0cfdbd05113967
Sean Belverstone <seanbelverstone@gmail.com>
2023-08-27 13:08:46 -0500

    added the band logo and gave it an animation. Installed react-transition-group as i may use it later

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import bandLogo from '../assets/bandImages/bandLogo.jpg';
+import './css/Home.css';
 
 function Home() {
 	return (
 		<div className="homePage">
-			{/* Navbar */}
-			{/* main logo */}
-			{/* description block */}
-			{/* facebook widget */}
+			<img src={bandLogo} id="bandLogo" alt="The logo for the band, Devil Rain. It's a white circle with two red droplets near the center. Below the droplets is the band name, with the text: Rock, blues and soul live music below it."/>
 		</div>
 	);
 }
