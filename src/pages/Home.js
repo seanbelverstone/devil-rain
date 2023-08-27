4d90cbc106b502d75ed3faeab5735bd24ab6fe2b
Sean Belverstone <seanbelverstone@gmail.com>
2023-08-26 19:05:17 -0500

    setup file structure and added main page files. Removed App.js and its style file and used Home as import instead

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div className="homePage">
+		{/* Navbar */}
+		{/* main logo */}
+		{/* description block */}
+		{/* facebook widget */}
+    </div>
+  );
+}
+
+export default Home;
