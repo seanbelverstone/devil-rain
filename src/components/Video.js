142978e8eb832db311f4a4966217a43571d8d4e1
Sean Belverstone <seanbelverstone@gmail.com>
2023-08-31 11:39:36 -0500

    managed to get the video to show up as an embed now

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,18 +4,33 @@ import PropTypes from 'prop-types';
 import './css/Video.css';
 import { isEmpty } from '../utils';
 
-export const Video = props => (
-	<div className={`${props.latest ? 'latestVideoItem' : 'videosItem'}`}>
-		<div className={`${props.latest ? 'latestVideoImage' : 'videoImage'}`}>
-			<a target="_blank" href={props.video.link} rel="noreferrer">
-				{!isEmpty(props.video.guid) && <img src={`https://i4.ytimg.com/vi/${props.video.guid.split(':')[2]}/mqdefault.jpg`} />}
-			</a>
+export const Video = props => {
+	const { latest } = props;
+	if (latest) {
+		return (
+			<iframe
+				width="600"
+				height="340"
+				src="https://www.youtube.com/embed?listType=user_uploads&list=WeHateMovies"
+				frameBorder="0"
+				allowfullscreen>
+			</iframe> 
+
+		);
+	}
+	return (
+		<div className="videosItem">
+			<div className="videoImage">
+				<a target="_blank" href={props.video.link} rel="noreferrer">
+					{!isEmpty(props.video.guid) && <img src={`https://i4.ytimg.com/vi/${props.video.guid.split(':')[2]}/mqdefault.jpg`} />}
+				</a>
+			</div>
+			<div className="videoFooter">
+				<p className="videoTitle">{props.video.title}</p>
+			</div>
 		</div>
-		<div className={`${props.latest ? 'latestVideoFooter' : 'videoFooter'}`}>
-			<p className={`${props.latest ? 'latestVideoTitle' : 'videoTitle'}`}>{props.video.title}</p>
-		</div>
-	</div>
-);
+	);
+};
 
 Video.propTypes = {
 	video: PropTypes.object,
