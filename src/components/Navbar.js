9e855ba78ebf7edfe5b066c39aac1b7f25f8d720
Sean Belverstone <seanbelverstone@gmail.com>
2023-09-05 17:24:35 -0500

    got the navbar to show the links correctly. Added slide out animations

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import facebookIcon from '../assets/facebookLogo.png';
@@ -62,11 +61,64 @@ function Navbar() {
 				</>
 			) : (
 				<>
-					<div className={`container${menuOpen ? ' change' : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
-						<div className="bar1"></div>
-						<div className="bar2"></div>
-						<div className="bar3"></div>
-					</div><div id="navNameContainer" style={{ justifyContent: 'flex-end' }}>
+					{menuOpen ? (
+						// Hamburger turns to X when menu is open
+						<div
+							className={`hamburgerMenu${menuOpen ? ' slideOut' : ' slideIn'}`}
+						>
+							<div
+								className={`container${menuOpen ? ' change' : ''}`}
+								onClick={() => setMenuOpen(!menuOpen)}
+								style={{
+									...(menuOpen ? {
+										transform: 'translateX(32vw)',
+										zIndex: 15
+									} : {}) }}
+							>
+								<div className="bar1"></div>
+								<div className="bar2"></div>
+								<div className="bar3"></div>
+							</div>
+							<div id="hamburgerNavLinkContainer">
+								<NavLink className="navlinks" to="/">
+									<span className="material-symbols-outlined navicon" id="home" alt="home">home</span>
+									<h3>Home</h3>
+								</NavLink>
+								<NavLink className="navlinks" to="/about">
+									<span className="material-symbols-outlined navicon" id="home" alt="home">info</span>
+									<h3>About Us</h3>
+								</NavLink>
+								<NavLink className="navlinks" to="/videos">
+									<span className="material-symbols-outlined navicon" id="videos">movie</span>
+									<h3>Videos</h3>
+								</NavLink>
+								<NavLink className="navlinks" to="/contact">
+									<span className="material-symbols-outlined navicon" id="contact">mail</span>
+									<h3>Contact</h3>
+								</NavLink>
+								<NavLink className="navlinks" to="/merch">
+									<span className="material-symbols-outlined navicon" id="merch">shopping_cart</span>
+									<h3>Merch</h3>
+								</NavLink>
+							</div>
+						</div>
+					) : (
+						// Closed menu, hamburger is lines
+						<div
+							className={`container${menuOpen ? ' change' : ''}`}
+							onClick={() => setMenuOpen(!menuOpen)}
+							style={{
+								...(menuOpen ? {
+									transform: 'translateX(40vw)',
+									zIndex: 15
+								} : {}) }}
+						>
+							<div className="bar1"></div>
+							<div className="bar2"></div>
+							<div className="bar3"></div>
+						</div>
+					)}
+					<div id="navNameContainer" style={{ justifyContent: 'flex-end' }}>
 						<img src={bandLogo} id="bandLogo" alt="" />
 						<div id="navNameBackground">
 							<img src={bandName} id="navName" alt="" />
