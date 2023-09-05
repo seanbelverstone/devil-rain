143511a36ce4e2e3a4cbe4b5c6af5ff2dfa4b6d9
Sean Belverstone <seanbelverstone@gmail.com>
2023-09-05 00:30:28 -0500

    created a hamburger symbol that changes from open to closed on click.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import facebookIcon from '../assets/facebookLogo.png';
 import bandLogo from '../assets/bandLogo.jpg';
@@ -6,11 +6,19 @@ import bandName from '../assets/bandName.png';
 import './css/Navbar.css';
 
 function Navbar() {
+	const [mobileView, setMobileView] = useState(false);
+	const [menuOpen, setMenuOpen] = useState(false);
 	const location = useLocation();
+	const screenWidth = document.documentElement.clientWidth;
+
 	useEffect(() => {
 		setHighlight();
 	}, [location.pathname]);
 
+	useEffect(() => {
+		setMobileView(screenWidth < 650);
+		console.log(screenWidth, mobileView);
+	}, []);
 
 	const setHighlight = () => {
 		Array.from(document.getElementsByClassName('navicon')).forEach(item => item.classList.remove('active'));
@@ -20,33 +28,51 @@ function Navbar() {
 	};
 
 	return (
-		<div className="navbar">
-			<div id="navNameContainer">
-				<img src={bandLogo} id="bandLogo" alt=""/>
-				<div id="navNameBackground">
-					<img src={bandName} id="navName" alt=""/>
-				</div>
-			</div>
-			<div id="navlinkContainer">
-				<NavLink className="navlinks" to="/">
-					<span className="material-symbols-outlined navicon" id="home" alt="home">home</span>
-				</NavLink>
-				<NavLink className="navlinks" to="/about">
-					<span className="material-symbols-outlined navicon" id="home" alt="home">info</span>
-				</NavLink>
-				<NavLink className="navlinks" to="/videos">
-					<span className="material-symbols-outlined navicon" id="videos">movie</span>
-				</NavLink>
-				<NavLink className="navlinks" to="/contact">
-					<span className="material-symbols-outlined navicon" id="contact">mail</span>
-				</NavLink>
-				<NavLink className="navlinks" to="/merch">
-					<span className="material-symbols-outlined navicon" id="merch">shopping_cart</span>
-				</NavLink>
-			</div>
-			<a href="https://www.facebook.com/groups/544578629743391" target="_blank" rel="noreferrer">
-				<img src={facebookIcon} alt="The Facebook icon" id="facebookIcon"/>
-			</a>
+		<div className="navbar" style={{ ...(mobileView ? { gap: 0 } : {})}}>
+
+			{!mobileView ? (
+				<>
+					<div id="navNameContainer">
+						<img src={bandLogo} id="bandLogo" alt=""/>
+						<div id="navNameBackground">
+							<img src={bandName} id="navName" alt=""/>
+						</div>
+					</div>
+					<div id="navlinkContainer">
+						<NavLink className="navlinks" to="/">
+							<span className="material-symbols-outlined navicon" id="home" alt="home">home</span>
+						</NavLink>
+						<NavLink className="navlinks" to="/about">
+							<span className="material-symbols-outlined navicon" id="home" alt="home">info</span>
+						</NavLink>
+						<NavLink className="navlinks" to="/videos">
+							<span className="material-symbols-outlined navicon" id="videos">movie</span>
+						</NavLink>
+						<NavLink className="navlinks" to="/contact">
+							<span className="material-symbols-outlined navicon" id="contact">mail</span>
+						</NavLink>
+						<NavLink className="navlinks" to="/merch">
+							<span className="material-symbols-outlined navicon" id="merch">shopping_cart</span>
+						</NavLink>
+					</div>
+					<a href="https://www.facebook.com/groups/544578629743391" target="_blank" rel="noreferrer">
+						<img src={facebookIcon} alt="The Facebook icon" id="facebookIcon" />
+					</a>
+				</>
+			) : (
+				<>
+					<div className={`container${menuOpen ? ' change' : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
+						<div className="bar1"></div>
+						<div className="bar2"></div>
+						<div className="bar3"></div>
+					</div><div id="navNameContainer" style={{ justifyContent: 'flex-end' }}>
+						<img src={bandLogo} id="bandLogo" alt="" />
+						<div id="navNameBackground">
+							<img src={bandName} id="navName" alt="" />
+						</div>
+					</div>
+				</>
+			)}
 		</div>
 	);
 }
