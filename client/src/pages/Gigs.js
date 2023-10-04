d144a543943a85162701c1733fdf31e2f642ade0
Sean Belverstone <seanbelverstone@gmail.com>
2023-10-04 17:18:32 -0500

    added the loading bar to the upcoming gigs page

--- client/src/pages/Gigs.js
+++ client/src/pages/Gigs.js
@@ -1,27 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './css/Home.css';
+import { LoadingIcon } from '../components/LoadingIcon';
 
 function Gigs() {
+	const [loading, setLoading] = useState(true);
 	const [gigList, setGigList] = useState([]);
 	useEffect(() => {
 		getGigs();
 	}, []);
 
 	const getGigs = () => {
+		setLoading(true);
 		return axios.get('/api/scrape').then(res => {
-			res.data.length > 0 && setGigList(res.data);
+			setGigList(res.data);
+			setTimeout(() => {
+				setLoading(false);
+			}, 3000);
 		});
 	};
-	return (
-		<div className="page">
-			<h1 className="title">Upcoming Gigs</h1>
-			{gigList.length > 0 
-				? gigList.map(gig => (
+	if (loading) {
+		return (
+			<div className="page">
+				<h1 className="title">Upcoming Gigs</h1>
+				<LoadingIcon />
+			</div>
+		);
+	}
+	if (!loading && gigList.length === 0) {
+		return (
+			<div className="page">
+				<h1 className="title">Upcoming Gigs</h1>
+				<h2>No upcoming gigs</h2>
+			</div>
+		);
+	} else {
+		return (
+			<div className="page">
+				<h1 className="title">Upcoming Gigs</h1>
+				{gigList.map(gig => (
 					<div key={gig.position}>{gig.date}</div>
-				)) : (<h2>No upcoming gigs</h2>)}
-		</div>
-	);
+				))}
+			</div>
+		);
+	}
 }
 
 export default Gigs;
