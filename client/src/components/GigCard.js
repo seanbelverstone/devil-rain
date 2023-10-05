c3fb43567cb56b1709043eeae9b5b4a826771525
Sean Belverstone <seanbelverstone@gmail.com>
2023-10-05 11:42:20 -0500

    turned the location into a googlemaps hyperlink

--- client/src/components/GigCard.js
+++ client/src/components/GigCard.js
@@ -3,10 +3,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import defaultImage from '../assets/defaultImage.jpg';
 import './css/GigCard.css';
+import { isEmpty } from '../utils';
 
 export const GigCard = props => {
 	const { gig } = props;
-	const { img, date, time, location, description } = gig;
+	const { img, date, time, location, description, type } = gig;
 
 	return (
 		<div className="gigCard">
@@ -17,12 +18,28 @@ export const GigCard = props => {
 			</div>
 			<div className="gigTime">{time}</div>
 			<div className="locationWrapper">
-				{location.split(',').map((loc, i) => (
-					<span key={`${loc}_${i}`} className={`location${i}`}>{loc}{i === 0 ? ', ' : ''}</span>
-				))}
+				{location.split(',').map((loc, i) => {
+					const queryParams = `${loc.split(' ').join('+')}+${location.split(', ')[1]}`;
+					// query params will be sent like The+Crooklets+Inn+Bude
+					if (i === 0 && loc !== 'Private Function') {
+						return (
+							<a
+								key={`${loc}_${i}`}
+								className={`location${i} link`}
+								href={`https://maps.google.com/maps?q=${queryParams}`}
+								target="_blank"
+								rel="noreferrer"
+							>
+								{`${loc}, `}
+							</a>
+						);
+					}
+					return (<span key={`${loc}_${i}`} className={`location${i}`}>{loc}</span>);
+				})}
 			</div>
 			<img className="gigImage" src={img === 'https://www.lemonrock.com/gx/1x1ph.png' ? defaultImage : img} />
-			<span className="description">{description}</span>
+			<span className="description">{!isEmpty(description) ? description : 'Check back for details!'}</span>
+			<span className="type" style={{ color: type === 'Private' ? 'grey' : 'var(--accent)'}}>{type}</span>
 		</div>
 	);
 };
@@ -38,6 +55,7 @@ GigCard.defaultProps = {
 		date: '',
 		time: '',
 		location: '',
-		description: ''
+		description: '',
+		type: ''
 	}
 };
