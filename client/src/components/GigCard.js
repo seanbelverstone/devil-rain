ef4f35488acafd86abeb4ddc91c5703ac939efb0
Sean Belverstone <seanbelverstone@gmail.com>
2023-10-05 11:19:55 -0500

    finished styling the gig cards for now

--- client/src/components/GigCard.js
+++ client/src/components/GigCard.js
@@ -16,10 +16,12 @@ export const GigCard = props => {
 				))}
 			</div>
 			<div className="gigTime">{time}</div>
-			{location.split(',').map((loc, i) => (
-				<span key={`${loc}_${i}`} className={`location${i}`}>{loc}{i === 0 ? ', ' : ''}</span>
-			))}
-			<img src={img === 'https://www.lemonrock.com/gx/1x1ph.png' ? defaultImage : img} />
+			<div className="locationWrapper">
+				{location.split(',').map((loc, i) => (
+					<span key={`${loc}_${i}`} className={`location${i}`}>{loc}{i === 0 ? ', ' : ''}</span>
+				))}
+			</div>
+			<img className="gigImage" src={img === 'https://www.lemonrock.com/gx/1x1ph.png' ? defaultImage : img} />
 			<span className="description">{description}</span>
 		</div>
 	);
