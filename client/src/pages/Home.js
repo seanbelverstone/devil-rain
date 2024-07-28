6f39f8c69431d079974184d2b504f993e8edd635
Sean Belverstone <seanbelverstone@gmail.com>
2024-07-28 16:40:02 -0500

    instead of using the band name with a separate background element i combined them. Added it to the page and added a logo on smaller screens

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import imageOne from '../assets/imageOne.jpg';
-import bandName from '../assets/bandName.png';
+import bandName from '../assets/bandNameEdit.png';
+import bandNameBackground from '../assets/bandNameBackground.png';
+import bandLogo from '../assets/bandLogo.jpg';
 import './css/Home.css';
 
 function Home() {
+	const [screenWidth, setScreenWidth] = useState(0);
+
+	useEffect(() => {
+		console.log(screenWidth);
+		const handleResize = () => {
+			setScreenWidth(window.innerWidth);
+		};
+		window.addEventListener('resize', handleResize);
+	});
+
 	return (
 		<div className="homePage">
 			<img src={imageOne} id="heroImage" alt=""/>
-			<div id="nameBackground" />
-			<img src={bandName} id="bandName" alt=""/>
+			<div id="bandNameSection" style={{ display: 'flex', flexDirection: 'row' }}>
+				<img src={bandNameBackground} className="bandNameBackground" alt=""/>
+				<img src={bandName} id="bandName" alt="" />
+				<img src={bandNameBackground} className="bandNameBackground" alt=""/>
+			</div>
+			<div id="logoSection">
+				<img src={bandLogo} className="homePageLogo" />
+			</div>
 		</div>
 	);
 }
